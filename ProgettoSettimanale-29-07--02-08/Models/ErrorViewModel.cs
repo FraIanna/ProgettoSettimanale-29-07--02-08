0869c6dff18337012b5b4529cc6ff4036175b510
FraIanna <francescoiannaccone9@gmail.com>
2024-07-29 17:07:31 +0200

    Aggiungere i file di progetto.

---
+++ ProgettoSettimanale-29-07--02-08/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgettoSettimanale_29_07__02_08.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
