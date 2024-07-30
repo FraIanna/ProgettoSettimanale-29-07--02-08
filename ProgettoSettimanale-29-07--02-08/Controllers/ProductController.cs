53e18c7fa34eeaf44712dec283889269c28dd325
FraIanna <francescoiannaccone9@gmail.com>
2024-07-30 16:57:04 +0200

    Update

---
+++ ProgettoSettimanale-29-07--02-08/Controllers/ProductController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProgettoSettimanale_29_07__02_08.Context;
+using ProgettoSettimanale_29_07__02_08.Models.Entities;
+using System.Collections.Generic;
+
+namespace ProgettoSettimanale_29_07__02_08.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly DataContext _dataContext;
+
+        public ProductController(DataContext dataContext, ILogger<ProductController> logger)
+        {
+            _logger = logger;
+            _dataContext = dataContext;
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _dataContext.Products.Any(e => e.Id == id);
+        }
+
+        public async Task<IActionResult> ProductList()
+        {
+            var products = await _dataContext.Products.ToListAsync();
+            return View(products);
+        }
+
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataContext.Add(product);
+                await _dataContext.SaveChangesAsync();
+                return RedirectToAction(nameof(ProductList));
+            }
+            return View(product);
+        }
+
+        public async Task<IActionResult> EditProduct(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _dataContext.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _dataContext.Update(product);
+                    await _dataContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(ProductList));
+            }
+            return View(product);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _dataContext.Products
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _dataContext.Products.FindAsync(id);
+            _dataContext.Products.Remove(product!);
+            await _dataContext.SaveChangesAsync();
+            return RedirectToAction(nameof(ProductList));
+        }
+
+        public async Task<IActionResult> ProductDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _dataContext.Products
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
